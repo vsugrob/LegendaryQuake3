d77b373d6a59c5e4d9ef2a148a9118335f6c7a10
Vitaliy Sugrobov <vsugrob@hotmail.com>
2025-09-16 13:21:15 +0300

    Set "g_legendary" to "1" by default

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -110,7 +110,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_gametype, "g_gametype", "0", CVAR_SERVERINFO | CVAR_USERINFO | CVAR_LATCH, 0, qfalse  },
 
 	// "Legendary" game mode (players spawned with random weapons)
-	{ &g_legendary, "g_legendary", "0", CVAR_SERVERINFO | CVAR_LATCH, 0, qfalse },
+	{ &g_legendary, "g_legendary", "1", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, 0, qfalse },
 
 	{ &g_maxclients, "sv_maxclients", "8", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, 0, qfalse  },
 	{ &g_maxGameClients, "g_maxGameClients", "0", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, 0, qfalse  },
